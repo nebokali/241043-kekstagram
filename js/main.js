9147120da32d3e393c8618bb8235f4345c890db6
nebokali <nebokali@gmail.com>
2018-12-19 19:10:43 +0300

    Валидация хештегов

--- js/main.js
+++ js/main.js
@@ -153,3 +153,45 @@ for (var l = 0; l < effectsRadio.length; l++) {
     }
   });
 }
+
+//Валидация формы
+var buttonUploadSubmit = document.querySelector('#upload-submit');
+var textHashtags = document.querySelector('.text__hashtags');
+
+function validateHashtagsForm() {
+  var hashtags = textHashtags.value.split(/[\s]+/);
+  for (var n = 0; n < hashtags.length; n++) {
+    var hashtag = hashtags[n];
+    if (hashtag !== '') {
+      var count = 0;
+      for (var m = 0; m < hashtags.length; m++) {
+        if (hashtag.toLowerCase() === hashtags[m].toLowerCase()) {
+          count++;
+        }
+      }
+      if (hashtag.charAt(0) !== '#') {
+        textHashtags.setCustomValidity('Хэштег должен начинаться с #');
+        break;
+      } else if (hashtag === '#') {
+        textHashtags.setCustomValidity('Хэштег не может состоять из одного #');
+        break;
+      } else if (hashtag.lastIndexOf('#') > 0) {
+        textHashtags.setCustomValidity('Хэштеги необходимо разделять пробелами');
+        break;
+      } else if (hashtags.length > 5) {
+        textHashtags.setCustomValidity('Нельзя использовать более пяти хэштегов');
+        break;
+      } else if (hashtag.length > 20) {
+        textHashtags.setCustomValidity('Максимальная длина хэштега 20 символов (знак # считается за один символ)');
+        break;
+      } else if (count > 1) {
+        textHashtags.setCustomValidity('Хештеги должны быть разными и не повторяться');
+        break;
+      } else {
+        textHashtags.setCustomValidity('');
+      }
+    }
+  }
+}
+
+buttonUploadSubmit.addEventListener('click', validateHashtagsForm());
