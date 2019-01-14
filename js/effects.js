5bf46e797f65700e29e08511fc9e4f3ea2eeba32
nebokali <nebokali@gmail.com>
2019-01-14 18:48:11 +0300

    Изменения

--- js/effects.js
+++ js/effects.js
@@ -11,6 +11,9 @@
   var effectLevelValue = document.querySelector('.effect-level__value');
   var imageUploadPreview = document.querySelector('.img-upload__preview img');
   var MAX_SLIDER_LENGTH = 453;
+  var MIN_VALUE = 25;
+  var MAX_VALUE = 100;
+  var STEP = 25;
 
   for (var l = 0; l < effectsRadio.length; l++) {
     effectsRadio[l].addEventListener('change', function (evt) {
@@ -125,6 +128,43 @@
   var templateError = document.querySelector('#error')
       .content
       .querySelector('.error');
+
+
+  var scaleControlSmaller = document.querySelector('.scale__control--smaller');
+  var scaleControlBigger = document.querySelector('.scale__control--bigger');
+  var scaleControlValue = document.querySelector('.scale__control--value');
+
+  var getScalePosition = function () {
+    var value = scaleControlValue.value;
+    return parseInt(value.replace(/\D/, ''), 10);
+  };
+
+  var setScalePosition = function (value) {
+    scaleControlValue.value = value + '%';
+    imageUploadPreview.style.transform = 'scale(' + value / 100 + ' )';
+  };
+
+  scaleControlBigger.addEventListener('click', function () {
+    var value = getScalePosition();
+
+    if ((value+STEP) < MAX_VALUE) {
+      value += STEP;
+      setScalePosition(value);
+    } else if ((value+STEP) >= MAX_VALUE) {
+      setScalePosition(100);
+    }
+  });
+
+  scaleControlSmaller.addEventListener('click', function () {
+    var value = getScalePosition();
+
+    if ((value-STEP) > MIN_VALUE) {
+      value -= STEP;
+      setScalePosition(value);
+    } else if ((value-STEP) <= MAX_VALUE) {
+      setScalePosition(25);
+    }
+  });
 
   window.effects = {
     setSliderPosition: setSliderPosition,
